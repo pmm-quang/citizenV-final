157f8d655c985f3fe9e4d8cfcf064d44e4a2df78
quan149 <19020399@vnu.edu.vn>
2023-05-18 13:35:14 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import Login from './Login/login';
-import Residential from './Residential/residential';
 import FindResidential from './Residential/FindResidential'
 import Province from './Management/Province';
 import DataEntry from './Residential/DataEntry';
